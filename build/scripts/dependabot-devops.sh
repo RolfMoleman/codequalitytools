4b20ac3297c70be301a397d54899ae7044e072bc
Carl Dawson <carl.dawson@bca.com>
2022-10-13 07:40:22 +0100

    added extension specific pipeline

--- build/scripts/dependabot-devops.sh
+++ build/scripts/dependabot-devops.sh
@@ -2,21 +2,30 @@
 ## below variables donet work whilst we have the old Azure DevOps url
 #SYSTEM_COLLECTIONURI_TRIM=`echo "${SYSTEM_COLLECTIONURI:22}"`
 #PROJECT_PATH="$SYSTEM_COLLECTIONURI_TRIM$SYSTEM_TEAMPROJECT/_git/$BUILD_REPOSITORY_NAME"
-## old Visula studio permitting variables
+## old Visual studio permitting variables
 
 if [[ "$SYSTEM_COLLECTIONURI" == *"dev.azure.com"* ]]; then
   echo "Using new URL"
   SYSTEM_COLLECTIONURI_TRIM=`echo "${SYSTEM_COLLECTIONURI:22}"`
+  
 elif [[ "$SYSTEM_COLLECTIONURI" == *"visualstudio.com"* ]]; then
-  echo "Using OLD URL"
-  SYSTEM_COLLECTIONURI_TRIM=`echo $SYSTEM_COLLECTIONURI | grep -oP '(?<=//).*(?=.visualstudio.com)'`
-  SYSTEM_COLLECTIONURI_TRIM+=/
+    echo "Using OLD URL"
+    SYSTEM_COLLECTIONURI_TRIM=`echo $SYSTEM_COLLECTIONURI | grep -oP '(?<=//).*(?=.visualstudio.com)'`
+    SYSTEM_COLLECTIONURI_TRIM+=/
+
+else
+  echo "URL not known"
+
 fi
 
 
 PROJECT_PATH="$SYSTEM_COLLECTIONURI_TRIM$SYSTEM_TEAMPROJECT/_git/$BUILD_REPOSITORY_NAME"
+PROJECT_PATH=`echo "${PROJECT_PATH,,}"`
+
+PROJECT=`echo "${SYSTEM_TEAMPROJECT,,}"`
+
 echo "org: $SYSTEM_COLLECTIONURI_TRIM"
-echo "project: $SYSTEM_TEAMPROJECT"
+echo "project: $PROJECT"
 echo "repo: $BUILD_REPOSITORY_NAME"
 echo "path: $PROJECT_PATH"
 
@@ -30,7 +39,7 @@ PARENTNAME="$(basename "$(dirname "$path")")"
 DIRECTORY_PATH="/"$PARENTNAME
 echo "directory: $DIRECTORY_PATH"
 echo "---[ Starting nuget dependabot run: $path ]---"
-echo `docker run  -v "$(pwd)/dependabot-script:/home/dependabot/dependabot-script" -w '/home/dependabot/dependabot-script' -e AZURE_ACCESS_TOKEN=$SYSTEM_ACCESSTOKEN -e PACKAGE_MANAGER='nuget' -e PROJECT_PATH=$PROJECT_PATH -e DIRECTORY_PATH=$DIRECTORY_PATH dependabot/dependabot-core bundle exec ruby ./generic-update-script.rb`
+echo `docker run  -v "$(pwd)/dependabot-script:/home/dependabot/dependabot-script" -w '/home/dependabot/dependabot-script' -e AZURE_ACCESS_TOKEN=$SYSTEM_ACCESSTOKEN -e PACKAGE_MANAGER='nuget' -e PROJECT_PATH=$PROJECT_PATH -e DIRECTORY_PATH=$DIRECTORY_PATH dependabot/dependabot-core:latest bundle exec ruby ./generic-update-script.rb`
 echo "---[ Finished nuget dependabot run ]---"
 done
 
@@ -45,10 +54,9 @@ PARENTNAME="$(basename "$(dirname "$path")")"
 DIRECTORY_PATH="/"$PARENTNAME
 echo "directory: $DIRECTORY_PATH"
 echo "---[ Starting terraform dependabot run: $path ]---"
-echo `docker run  -v "$(pwd)/dependabot-script:/home/dependabot/dependabot-script" -w '/home/dependabot/dependabot-script' -e AZURE_ACCESS_TOKEN=$SYSTEM_ACCESSTOKEN -e PACKAGE_MANAGER='maven' -e PROJECT_PATH=$PROJECT_PATH -e DIRECTORY_PATH=$DIRECTORY_PATH dependabot/dependabot-core bundle exec ruby ./generic-update-script.rb`
+echo `docker run  -v "$(pwd)/dependabot-script:/home/dependabot/dependabot-script" -w '/home/dependabot/dependabot-script' -e AZURE_ACCESS_TOKEN=$SYSTEM_ACCESSTOKEN -e PACKAGE_MANAGER='maven' -e PROJECT_PATH=$PROJECT_PATH -e DIRECTORY_PATH=$DIRECTORY_PATH dependabot/dependabot-core:latest bundle exec ruby ./generic-update-script.rb`
 echo "---[ Finished Maven dependabot run ]---"
 done
-
 
 
 echo "---[ Preparing for Terraform dependabot run ]---"
@@ -61,6 +69,7 @@ PARENTNAME="$(basename "$(dirname "$path")")"
 DIRECTORY_PATH="/"$PARENTNAME
 echo "directory: $DIRECTORY_PATH"
 echo "---[ Starting terraform dependabot run: $path ]---"
-echo `docker run  -v "$(pwd)/dependabot-script:/home/dependabot/dependabot-script" -w '/home/dependabot/dependabot-script' -e AZURE_ACCESS_TOKEN=$SYSTEM_ACCESSTOKEN -e PACKAGE_MANAGER='terraform' -e PROJECT_PATH=$PROJECT_PATH -e DIRECTORY_PATH=$DIRECTORY_PATH dependabot/dependabot-core bundle exec ruby ./generic-update-script.rb`
+echo `docker run  -v "$(pwd)/dependabot-script:/home/dependabot/dependabot-script" -w '/home/dependabot/dependabot-script' -e AZURE_ACCESS_TOKEN=$SYSTEM_ACCESSTOKEN -e PACKAGE_MANAGER='terraform' -e PROJECT_PATH=$PROJECT_PATH -e DIRECTORY_PATH=$DIRECTORY_PATH dependabot/dependabot-core:latest bundle exec ruby ./generic-update-script.rb`
 echo "---[ Finished Terraform dependabot run ]---"
 done
+
